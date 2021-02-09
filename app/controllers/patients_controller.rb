f69abca328a6525e97b4e05d7ef85dc53d7d8cee
anthony mcmullen <amcmullen9291@gmail.com>
2021-02-09 21:37:07 +0000

    Automatically backed up by Learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,10 @@
 class PatientsController < ApplicationController
+  
+  def index
+        @patient = Patient.new
+    end
+
+    def show
+        @patient = Patient.find(params[:id])
+    end
 end
